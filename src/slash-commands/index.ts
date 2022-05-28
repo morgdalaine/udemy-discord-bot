af74d19711892bbcedca68eb9b799b371bd9d095
morgdalaine <morgdalaine@gmail.com>
2022-05-28 17:36:49 -0500

    add get_random_anime slash command

--- src/slash-commands/index.ts
+++ src/slash-commands/index.ts
@@ -1,5 +1,6 @@
 import { SlashCommand } from '../types';
 import { AddCommand } from './add';
+import { GetRandomAnimeCommand } from './get-random-anime';
 import { GoogleCommand } from './google';
 import { HelloCommand } from './hello';
 import { PollCommand } from './poll';
@@ -7,6 +8,7 @@ import { UserInfoCommand } from './user-info';
 
 export const SlashCommands: SlashCommand[] = [
   AddCommand,
+  GetRandomAnimeCommand,
   GoogleCommand,
   HelloCommand,
   PollCommand,
